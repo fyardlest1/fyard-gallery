dbc2c84c9decd4bbfd196c7b3efedcf67a549b43
fyardlest <fyardlest1@gmail.com>
2021-01-20 07:15:07 -0500

    Open Modal Setup

--- app.js
+++ app.js
@@ -7,3 +7,31 @@ function getElement(selection) {
     `Please check "${selection}" selector, no such element exists`
   );
 }
+
+function Gallery(element) {
+  this.container = element
+  this.list = [...element.querySelectorAll('.img')]
+  // target
+  this.modal = getElement('.modal')
+  this.modalImg = getElement('.main-img')
+  this.modalImages = getElement('.modal-images')
+  this.closeBtn = getElement('.close-btn')
+  this.nextBtn = getElement('.next-btn')
+  this.prevBtn = getElement('.prev-btn')
+  // let self = this
+  // bind functions
+  // this.openModal = this.openModal.bind(this)
+  // container event
+  this.container.addEventListener('click', function(e) {
+      // self.openModal()
+      this.openModal()
+    }.bind(this)
+  )
+}
+
+Gallery.prototype.openModal = function() {
+  this.modal.classList.add('open')
+}
+
+const nature = new Gallery(getElement('.nature'))
+const city = new Gallery(getElement('.city'))
