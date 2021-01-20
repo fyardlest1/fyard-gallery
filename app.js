7f1d168be6bb63381e7992656f84c62467840911
fyardlest <fyardlest1@gmail.com>
2021-01-20 07:37:01 -0500

    Open Modal Complete

--- app.js
+++ app.js
@@ -14,6 +14,7 @@ function Gallery(element) {
   // target
   this.modal = getElement('.modal')
   this.modalImg = getElement('.main-img')
+  this.imageName = getElement('.image-name')
   this.modalImages = getElement('.modal-images')
   this.closeBtn = getElement('.close-btn')
   this.nextBtn = getElement('.next-btn')
@@ -23,14 +24,27 @@ function Gallery(element) {
   // this.openModal = this.openModal.bind(this)
   // container event
   this.container.addEventListener('click', function(e) {
+    if (e.target.classList.contains('img')) {
+      this.openModal(e.target, this.list)
+    }
       // self.openModal()
-      this.openModal()
     }.bind(this)
   )
 }
 
-Gallery.prototype.openModal = function() {
+Gallery.prototype.openModal = function(selectedImage, list) {
+  this.setMainImage(selectedImage)
+  this.modalImages.innerHTML = list.map(function(image) {
+    return `<img src='${image.src}' title='${image.title}' data-id='${image.dataset.id}'
+    class='${selectedImage.dataset.id === image.dataset.id?'modal-img selected'
+    :'modal-img'}' />`
+  }).join('')
   this.modal.classList.add('open')
+}
+
+Gallery.prototype.setMainImage = function(selectedImage) {
+  this.modalImg.src = selectedImage.src
+  this.imageName.textContent = selectedImage.title
 }
 
 const nature = new Gallery(getElement('.nature'))
