e540843226867627aadd128c57b14bf2c77b89b4
fyardlest <fyardlest1@gmail.com>
2021-01-20 19:16:34 -0500

    Close Modal

--- app.js
+++ app.js
@@ -22,6 +22,9 @@ function Gallery(element) {
   // let self = this
   // bind functions
   // this.openModal = this.openModal.bind(this)
+  this.closeModal = this.closeModal.bind(this)
+  this.nextImage = this.nextImage.bind(this)
+  this.prevImage = this.prevImage.bind(this)
   // container event
   this.container.addEventListener('click', function(e) {
     if (e.target.classList.contains('img')) {
@@ -40,12 +43,25 @@ Gallery.prototype.openModal = function(selectedImage, list) {
     :'modal-img'}' />`
   }).join('')
   this.modal.classList.add('open')
+  this.closeBtn.addEventListener('click', this.closeModal)
+  this.nextBtn.addEventListener('click', this.nextImage)
+  this.prevBtn.addEventListener('click', this.prevImage)
 }
 
 Gallery.prototype.setMainImage = function(selectedImage) {
   this.modalImg.src = selectedImage.src
   this.imageName.textContent = selectedImage.title
 }
+
+Gallery.prototype.closeModal = function() {
+  this.modal.classList.remove('open')
+  this.closeBtn.removeEventListener('click', this.closeModal)
+  this.nextBtn.removeEventListener('click', this.nextImage)
+  this.prevBtn.removeEventListener('click', this.prevImage)
+}
+
+Gallery.prototype.nextImage = function() {}
+Gallery.prototype.prevImage = function() {}
 
 const nature = new Gallery(getElement('.nature'))
 const city = new Gallery(getElement('.city'))
